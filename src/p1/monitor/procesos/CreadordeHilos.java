7bce7bdd9ca120ceeea7e55def05869392c0d986
SAM <SAM@192.168.1.72>
2016-04-04 20:41:04 -0500

    Subida a GITHUB

---
+++ src/p1/monitor/procesos/CreadordeHilos.java
@@ -0,0 +1,33 @@
+package p1.monitor.procesos;
+
+import java.util.ArrayList;
+
+
+public class CreadordeHilos implements Runnable
+{
+ Thread hilo;
+ ArrayList<Proceso> ListaProcesos=new ArrayList<Proceso>();
+ 
+ public CreadordeHilos()
+ {
+  hilo.start();
+ }
+ 
+ 
+ 
+ //////////////////////////////////////////////////////////////////
+ public void run()
+ {
+  
+  
+  
+ }
+ 
+//////////////////////////////////////////////////////////////////
+ public void imprimeInicio()
+ {
+  
+ }
+ 
+ 
+}
---
+++ src/p1/monitor/procesos/Proceso.java
@@ -0,0 +1,31 @@
+package p1.monitor.procesos;
+
+
+public class Proceso implements Runnable
+{
+ static int contador=1;
+ 
+ private int numero;
+	
+ public Proceso()
+ {
+  numero=contador;
+  contador++;
+ }
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ public void run()
+ {
+	
+	
+ }
+ 
+}
